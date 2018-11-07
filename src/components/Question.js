b4f68e432652e2f867da63f88e2afd9b0cf04d61
Alexey Belov <alexey.belov@nordigy.ru>
2018-11-07 21:07:28 +0300

    develop question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,16 +3,42 @@ import { connect } from 'react-redux';
 import { formatQuestion } from '../utils/helpers';
 
 class Question extends Component {
+  toQuestion = (e, id) => {
+    e.preventDefault();
+    //TODO: redirect to Question
+  };
   render() {
-    console.log(this.props);
-    return <div className="question" />;
+    const { question } = this.props;
+    if (question === null) {
+      return <p>This Question doesn't exist</p>;
+    }
+    const { name, authorId, id, avatar, optionOne, optionTwo } = question;
+    console.log(optionOne);
+
+    return (
+      <div className="question">
+        <div className="question-info">{name} asks:</div>
+        <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
+        <div>
+          <span>Would you rather</span>
+          <span>
+            ...
+            {optionOne.text}
+            ...
+          </span>
+        </div>
+        <button className="btn" onClick={e => this.toQuestion(e, id)}>
+          View Poll
+        </button>
+      </div>
+    );
   }
 }
 
 function mapStateToProps({ users, questions }, { id }) {
   const question = questions[id];
   return {
-    question: formatQuestion(question, users[question.author]),
+    question: question ? formatQuestion(question, users[question.author]) : null,
   };
 }
 
