6850df2d05003583c7871ecddd6d8b00aef687fd
Alexey Belov <alexey.belov@nordigy.ru>
2018-11-23 21:40:14 +0300

    add css to leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -7,20 +7,29 @@ class LeaderBoard extends Component {
       return <Redirect to="/login" />;
     }
     return (
-      <div>
-        <ul className="center">
+      <div className="leader-list">
+        <ul>
           {Object.values(this.props.users).map(user => (
             <li key={user.id}>
-              <div>
-                <h3>{user.name}</h3>
-                <img src={user.avatarURL} alt={`Avatar of ${user.name}`} className="avatar-big" />
-                <div>
-                  <span>Answered questions {Object.keys(user.answers).length}</span>
-                  <span>Created questions {user.questions.length}</span>
+              <div className="leader-section">
+                <div className="avatar-section">
+                  <img src={user.avatarURL} alt={`Avatar of ${user.name}`} className="avatar-big" />
                 </div>
-                <div>
-                  <span>Score</span>
-                  <span>{Object.keys(user.answers).length + user.questions.length}</span>
+                <div className="questions-section">
+                  <div className="leaderboard-name">{user.name}</div>
+                  <div className="leaderboard-questions">
+                    Answered questions {Object.keys(user.answers).length}
+                  </div>
+                  <div className="line"> </div>
+                  <div className="leaderboard-questions">
+                    Created questions {user.questions.length}
+                  </div>
+                </div>
+                <div className="leaderboard-score">
+                  <div className="leaderboard-score-header">Score</div>
+                  <div className="leaderboard-score-body">
+                    {Object.keys(user.answers).length + user.questions.length}
+                  </div>
                 </div>
               </div>
             </li>
