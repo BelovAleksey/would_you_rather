ff0ad3f62706ce1c608c297f3a1c85856f53d863
Alexey Belov <alexey.belov@nordigy.ru>
2018-11-30 11:35:19 +0300

    improve component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { formatQuestion } from '../utils/helpers';
 import { Link } from 'react-router-dom';
 
-class Question extends Component {
-  render() {
-    const { name, id, avatar, optionOne } = this.props.question;
-    return (
-      <div className="question">
-        <div className="question-header">{name} asks:</div>
-        <div className="question-body">
-          <div className="avatar-section">
-            <span className="helper" />
-            <img src={avatar} alt={`Avatar of ${name}`} className="avatar-big" />
+function Question(props) {
+  const { name, id, avatar, optionOne } = props.question;
+  return (
+    <div className="question">
+      <div className="question-header">{name} asks:</div>
+      <div className="question-body">
+        <div className="avatar-section">
+          <span className="helper" />
+          <img src={avatar} alt={`Avatar of ${name}`} className="avatar-big" />
+        </div>
+        <div className="line" />
+        <div className="polling-section">
+          <div className="polling-header">Would you rather</div>
+          <div className="polling-body">
+            ...
+            {optionOne.text}
+            ...
           </div>
-          <div className="line" />
-          <div className="polling-section">
-            <div className="polling-header">Would you rather</div>
-            <div className="polling-body">
-              ...
-              {optionOne.text}
-              ...
-            </div>
-            <Link className="link" to={`question/${id}`}>
-              <div className="polling-btn">View Poll</div>
-            </Link>
-          </div>
+          <Link className="link" to={`question/${id}`}>
+            <div className="polling-btn">View Poll</div>
+          </Link>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 function mapStateToProps({ users, questions }, { id }) {
